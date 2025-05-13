4905f2b75db95ef4b516b72f047eb08f768e152e
Colm Murphy <colm@colmmurphy.xyz>
2025-05-13 16:10:50 +0100

    Fix clangd loading

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,15 +1,20 @@
 -- Ensure Mason and Mason-LSPConfig are setup
 require("mason").setup()
-require("mason-lspconfig").setup {
-  ensure_installed = { "clangd" },
-}
+require("mason-lspconfig").setup()
 
 -- LSP Config for clangd
 local lspconfig = require("lspconfig")
 
-lspconfig.clangd.setup {
-  cmd = { "clangd", "--background-index" },
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-  root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git")
-}
+local function is_c_compiler_available()
+  return vim.fn.executable("gcc") == 1 or vim.fn.executable("clang") == 1
+end
+
+-- only load clangd if C compiler in $PATH
+if is_c_compiler_available() then
+  lspconfig.clangd.setup {
+    cmd = { "clangd", "--background-index" },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git")
+  }
+end
 
